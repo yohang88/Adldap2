ff8fb29aa247b32fa6c9274ff5084b092f729166
Yoga Hanggara <yohang88@gmail.com>
2016-06-23 22:49:21 +0700

    fix getDistinguishedName attribute array

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -494,7 +494,7 @@ abstract class AbstractModel implements ArrayAccess, JsonSerializable
      */
     public function getDistinguishedName()
     {
-        return $this->getAttribute($this->schema->distinguishedName(), 0);
+        return $this->getAttribute($this->schema->distinguishedName());
     }
 
     /**
